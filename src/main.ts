887316df4bfa40c6d6fd4069242ac244e4528704
billyg83 <bgilmore83@hotmail.co.uk>
2025-08-23 15:46:31 +0100

    docs(swagger): user Get

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,19 @@ async function bootstrap() {
       },
     }),
   );
+
+  const serverUrl = 'http://localhost:8000';
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Nest JS rox')
+    .setDescription(`base API URL is ${serverUrl}`)
+    .setLicense('MIT Open Source', 'https://opensource.org/license/mit')
+    .setTermsOfService('https://developers.google.com/terms')
+    .addServer(serverUrl)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // 'api' is url param for swagger docs
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
--- src/users/dtos/getUsersParam.dto.ts
+++ src/users/dtos/getUsersParam.dto.ts
@@ -1,7 +1,12 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsInt } from 'class-validator';
 
 export class GetUsersParamDto {
+  @ApiPropertyOptional({
+    description: 'Get user with a specific Id, or get all users',
+    example: 1234,
+  })
   @IsOptional()
   @IsInt()
-  id: number;
+  id?: number;
 }
