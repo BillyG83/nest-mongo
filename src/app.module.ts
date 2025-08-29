d570d71f68b1b45ee7bc3239cb9acbb31d1a181a
billyg83 <bgilmore83@hotmail.co.uk>
2025-08-29 11:51:37 +0100

    feat: type ORM and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [AuthModule, PostsModule, UsersModule],
+  imports: [
+    AuthModule,
+    PostsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        database: 'nestjs-blog',
+        entities: [User],
+        host: 'localhost',
+        password: 'bill',
+        port: 5432,
+        synchronize: true, // should not be true in prod
+        type: 'postgres',
+        username: 'postgres',
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
